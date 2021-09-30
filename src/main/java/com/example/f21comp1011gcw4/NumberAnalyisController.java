c4778f3061c5efa5bd4a8b54ef721d20201ad8a1
JaretWright <jaret.wright@georgiancollege.ca>
2021-09-30 10:32:57 -0400

    added logic for 10 digit validation of a basic phone number and css

--- src/main/java/com/example/f21comp1011gcw4/NumberAnalyisController.java
+++ src/main/java/com/example/f21comp1011gcw4/NumberAnalyisController.java
@@ -1,4 +1,40 @@
 package com.example.f21comp1011gcw4;
 
-public class NumberAnalyisController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NumberAnalyisController implements Initializable {
+
+    @FXML
+    private TextField phoneNumberTextField;
+
+    @FXML
+    private Label areaCodeLabel;
+
+    @FXML
+    private Label cityCodeLabel;
+
+    @FXML
+    private Label msgLabel;
+
+    private void assessPhoneNumber()
+    {
+        String phoneNumber = phoneNumberTextField.getText();
+        if (phoneNumber.matches("[2-9]\\d{2}[2-9]\\d{2}\\d{4}"))
+            msgLabel.setText("valid phone number");
+        else
+            msgLabel.setText("invalid phone number");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        phoneNumberTextField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            assessPhoneNumber();
+        });
+    }
 }
